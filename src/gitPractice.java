31b2408abfdbbb32445f85d895915fc950096d9e
Walid Shahbaz <walidshahbaz@linas-MacBook-Pro.local>
2022-06-21 23:38:17 -0400

    this is my first git practice

---
+++ src/gitPractice.java
@@ -0,0 +1,12 @@
+
+public class gitPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("this is my first git practice");
+		
+	}
+
+}
